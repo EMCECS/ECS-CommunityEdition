773e47d77996cf6e8a1c1c23b75f4f6d888060f1
Patrick Butler Monterde <pbutlerm@gmail.com>
2015-06-19 00:04:20 -0700

    removed logging depenency

--- ecs-multi-node/settings.py
+++ ecs-multi-node/settings.py
@@ -34,36 +34,3 @@ ECS_SINGLENODE_LOGGING = {
     }
 }
 
-CONTAINER_LOGGING = {
-    'version': 1,
-    'disable_existing_loggers': False,
-    'formatters': {
-        'verbose': {
-            'format' : "[%(asctime)s] %(levelname)s [%(name)s:%(lineno)s] %(message)s",
-            'datefmt' : "%d/%b/%Y %H:%M:%S"
-        },
-        'simple': {
-            'format': '%(levelname)s %(message)s'
-        },
-    },
-    'handlers': {
-        'console': {
-            'level': 'DEBUG',
-            'class': 'logging.StreamHandler',
-            'formatter': 'verbose'
-        },
-         'file': {
-            'level': 'DEBUG',
-            'class': 'logging.FileHandler',
-            'formatter': 'verbose',
-            'filename' : 'modify-container-config.log'
-        }
-    },
-    'loggers': {
-        'root': {
-            'handlers': ['console', 'file'],
-            'propagate': True,
-            'level': 'DEBUG'
-        }
-    }
-}
