0cc72591f842ded040302f8ed78c61326cd288b3
Karsten Bott <Karsten.Bott@bottnet.de>
2015-09-28 10:42:28 +0200

    cm.opject.properties and common.object.properties
    inserted common.onject.properties retrieval for sed modification on ECS Singlenode

--- ecs-single-node/step1_ecs_singlenode_install.py
+++ ecs-single-node/step1_ecs_singlenode_install.py
@@ -462,7 +462,6 @@ def modify_container_conf_func():
         os.system(
             "docker exec -t  ecsstandalone cp /opt/storageos/conf/cm.object.properties /opt/storageos/conf/cm.object.properties.old")
 
-
         logger.info("Backup application config file")
         os.system(
             "docker exec -t  ecsstandalone cp /opt/storageos/ecsportal/conf/application.conf /opt/storageos/ecsportal/conf/application.conf.old")
@@ -470,6 +469,10 @@ def modify_container_conf_func():
         logger.info("Copy object properties files to host")
         os.system(
             "docker exec -t ecsstandalone cp /opt/storageos/conf/cm.object.properties /host/cm.object.properties1")
+            
+        logger.info("Copy object properties files to host")
+        os.system(
+            "docker exec -t ecsstandalone cp /opt/storageos/conf/common.object.properties /host/common.object.properties1")            
 
         logger.info("Copy application config file to host")
         os.system(
@@ -489,6 +492,10 @@ def modify_container_conf_func():
         logger.info("Copy modified files to container")
         os.system(
             "docker exec -t  ecsstandalone cp /host/cm.object.properties /opt/storageos/conf/cm.object.properties")
+            
+        logger.info("Copy modified files to container")
+        os.system(
+            "docker exec -t  ecsstandalone cp /host/common.object.properties /opt/storageos/conf/common.object.properties")
 
         os.system(
             "docker exec -t  ecsstandalone cp /host/application.conf /opt/storageos/ecsportal/conf/application.conf")
@@ -501,6 +508,7 @@ def modify_container_conf_func():
 
         logger.info("Clean up local files")
         os.system("rm -rf /host/cm.object.properties*")
+        os.system("rm -rf /host/commmon.object.properties*")
         os.system("rm -rf /host/application.conf")
 
 
