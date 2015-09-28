0f5601c32b8d8dbb55d702d26681eb2633309b77
Karsten Bott <Karsten.Bott@bottnet.de>
2015-09-28 12:46:55 +0200

    Update step1_ecs_singlenode_install.py
    fixed common.object.properties

--- ecs-single-node/step1_ecs_singlenode_install.py
+++ ecs-single-node/step1_ecs_singlenode_install.py
@@ -461,7 +461,9 @@ def modify_container_conf_func():
         logger.info("Backup object properties files")
         os.system(
             "docker exec -t  ecsstandalone cp /opt/storageos/conf/cm.object.properties /opt/storageos/conf/cm.object.properties.old")
-
+        
+        os.system(
+            "docker exec -t  ecsstandalone cp /opt/storageos/conf/common.object.properties /opt/storageos/conf/common.object.properties.old")
 
         logger.info("Backup application config file")
         os.system(
@@ -470,7 +472,10 @@ def modify_container_conf_func():
         logger.info("Copy object properties files to host")
         os.system(
             "docker exec -t ecsstandalone cp /opt/storageos/conf/cm.object.properties /host/cm.object.properties1")
-
+        
+        os.system(
+            "docker exec -t ecsstandalone cp /opt/storageos/conf/common.object.properties /host/common.object.properties1")    
+        
         logger.info("Copy application config file to host")
         os.system(
             "docker exec -t  ecsstandalone cp /opt/storageos/ecsportal/conf/application.conf /host/application.conf")
@@ -489,6 +494,9 @@ def modify_container_conf_func():
         logger.info("Copy modified files to container")
         os.system(
             "docker exec -t  ecsstandalone cp /host/cm.object.properties /opt/storageos/conf/cm.object.properties")
+        
+        os.system(
+            "docker exec -t  ecsstandalone cp /host/common.object.properties /opt/storageos/conf/common.object.properties")
 
         os.system(
             "docker exec -t  ecsstandalone cp /host/application.conf /opt/storageos/ecsportal/conf/application.conf")
@@ -500,6 +508,7 @@ def modify_container_conf_func():
         os.system("docker start ecsstandalone")
 
         logger.info("Clean up local files")
+        os.system("rm -rf /host/common.object.properties*")
         os.system("rm -rf /host/cm.object.properties*")
         os.system("rm -rf /host/application.conf")
 
