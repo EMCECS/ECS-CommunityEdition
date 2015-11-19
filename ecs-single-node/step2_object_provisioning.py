0ab0a0a560532b445e508d92c24c29bc913d77aa
petera5 <Aaron.Peterson@emc.com>
2015-11-19 10:59:04 -0600

    Increased sleep duration to match longer storage pool init times.

--- ecs-single-node/step2_object_provisioning.py
+++ ecs-single-node/step2_object_provisioning.py
@@ -293,7 +293,7 @@ def main(argv):
         
         for node in ECSNodeList:
                 CreateDataStoreOnCommodityNodesWithRetry(node, DataStoreName, ObjectVArrayID)
-        time.sleep(20 * 60)
+        time.sleep(120 * 60)
         InsertVDC(ECSNode, VDCName)
         print("VDCID: %s" %getVDCID(ECSNode, VDCName))
         CreateObjectVpoolWithRetry(ECSNode, ObjectVPool, VDCName)
