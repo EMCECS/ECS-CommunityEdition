5661db31942ba580fcb2889fbc8f3867dbf2c5ec
Travis <padthaitofuhot@gmail.com>
2017-05-10 19:30:18 -0400

    add some copyright headers remove some old contrib scripts

--- ui/resources/docker/torrent.sh
+++ ui/resources/docker/torrent.sh
@@ -1,4 +1,12 @@
 #!/usr/bin/env ash
+# Copyright (c) 2015 EMC Corporation
+# All Rights Reserved
+#
+# This software contains the intellectual property of EMC Corporation
+# or is licensed to EMC Corporation from third parties.  Use of this
+# software and the intellectual property contained therein is expressly
+# limited to the terms and conditions of the License Agreement under which
+# it is provided by or on behalf of EMC.
 
 # Start torrent tracker and seeder
 torrent() {
