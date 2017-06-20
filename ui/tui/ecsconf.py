b818a546db656fba21710cb45e668df8b56e008b
Travis Wichert <padthaitofuhot@gmail.com>
2017-06-20 09:53:42 -0400

    add logging

--- ui/tui/ecsconf.py
+++ ui/tui/ecsconf.py
@@ -9,10 +9,12 @@
 # it is provided by or on behalf of EMC.
 
 import os
+import sys
 import string
 import random
 import logging
 from defaults import *
+from tools import logobj
 
 logging.basicConfig(filename=ui_log, level=logging.DEBUG)
 logging.debug('-' * 40 + os.path.abspath(__file__) + '-' * 40)
@@ -169,6 +171,8 @@ DEFAULTS[EXPORT] = {
     DESC: DESC_DEFAULT
 }
 
+logobj(DEFAULTS)
+
 
 class ECSConf(object):
     """
@@ -176,7 +180,7 @@ class ECSConf(object):
     """
 
     def __init__(self, deploymap):
-
+        logging.debug(self.__class__.__name__ + ': ' + sys._getframe().f_code.co_name)
         self.deploy = deploymap
 
     @staticmethod
@@ -192,6 +196,10 @@ class ECSConf(object):
         """
         Access common patterns within the ECSConf object
         """
+        logging.debug(self.__class__.__name__ + ': ' + sys._getframe().f_code.co_name)
+        logobj(map_type)
+        logobj(key)
+        logobj(name)
         try:
             # If only map_type provided, return the data at map_type in the yaml tree
             if key is None and name is None:
@@ -218,7 +226,7 @@ class ECSConf(object):
         Returns a dict of Ansible facts
         :return: dict of facts
         """
-
+        logging.debug(self.__class__.__name__ + ': ' + sys._getframe().f_code.co_name)
         facts = {}
         facts.update(self.get_defaults(ANSIBLE))
 
@@ -241,6 +249,7 @@ class ECSConf(object):
         Returns a dict of important general facts
         :return: dict of facts
         """
+        logging.debug(self.__class__.__name__ + ': ' + sys._getframe().f_code.co_name)
         fun_facts = {}
 
         for key in FUN_FACTS:
@@ -253,24 +262,28 @@ class ECSConf(object):
         """
         Returns a list of name keys from the given map_type
         """
+        logging.debug(self.__class__.__name__ + ': ' + sys._getframe().f_code.co_name)
         return self.get_attr(map_type, key)
 
     def get_members(self, map_type, name, key=MEMBERS):
         """
         Returns a list of items from the members key of the given map_type and name
         """
+        logging.debug(self.__class__.__name__ + ': ' + sys._getframe().f_code.co_name)
         return self.get_attr(map_type, key, name)
 
     def get_root_user(self):
         """
         Returns the configured root user for the ECS deployemnt
         """
+        logging.debug(self.__class__.__name__ + ': ' + sys._getframe().f_code.co_name)
         return self.get_node_defaults()[ROOT_USER]
 
     def get_root_pass(self):
         """
         Returns the configured root password for the ECS deployment
         """
+        logging.debug(self.__class__.__name__ + ': ' + sys._getframe().f_code.co_name)
         return self.get_node_defaults()[ROOT_PASS]
 
     @staticmethod
@@ -282,6 +295,7 @@ class ECSConf(object):
         :param charset: Set of characters from which to choose randomly
         :returns: Random string
         """
+        logging.debug(self.__class__.__name__ + ': ' + sys._getframe().f_code.co_name)
         if charset is None:
             charset = '{}{}{}'.format(string.ascii_uppercase, string.ascii_lowercase, string.digits)
         return ''.join(random.SystemRandom().choice(charset) for _ in range(length))
@@ -290,6 +304,7 @@ class ECSConf(object):
         """
         Returns a dict of default options for the map type
         """
+        logging.debug(self.__class__.__name__ + ': ' + sys._getframe().f_code.co_name)
         result = {}
         result.update(DEFAULTS[map_type])
         map_type_d = map_type[:-1] + _D
@@ -305,6 +320,7 @@ class ECSConf(object):
         :param node: ansible_hostname of the node
         :returns: pool name or None
         """
+        logging.debug(self.__class__.__name__ + ': ' + sys._getframe().f_code.co_name)
         for sp in self.get_sp_names():
             if node in self.get_sp_members(sp):
                 return sp
@@ -316,6 +332,7 @@ class ECSConf(object):
         :param node: ansible_hostname of the node
         :return: vdc name or None
         """
+        logging.debug(self.__class__.__name__ + ': ' + sys._getframe().f_code.co_name)
         pool = self.get_node_pool(node)
         for vdc in self.get_vdc_names():
             if pool in self.get_vdc_members(vdc):
@@ -330,6 +347,7 @@ class ECSConf(object):
         # opts = {}
         # if NODE_DEFAULTS in self.deploy.facts:
         #     opts.update(self.deploy.facts[NODE_DEFAULTS].toDict())
+        logging.debug(self.__class__.__name__ + ': ' + sys._getframe().f_code.co_name)
         return self.get_defaults(NODE)
 
     def get_node_options(self, node):
@@ -338,6 +356,7 @@ class ECSConf(object):
         :param node: ansible_hostname of the node
         :returns: dict of node options or blank dict
         """
+        logging.debug(self.__class__.__name__ + ': ' + sys._getframe().f_code.co_name)
         opts = {}
 
         opts.update(self.get_node_defaults())
@@ -360,6 +379,7 @@ class ECSConf(object):
         Returns a list of all nodes of any type in deploy.yml
         :return:
         """
+        logging.debug(self.__class__.__name__ + ': ' + sys._getframe().f_code.co_name)
         nodes = [self.deploy.facts[INSTALL_NODE]]
         nodes += self.list_all_sp_nodes()
         return nodes
@@ -368,6 +388,7 @@ class ECSConf(object):
         """
         Returns a list of all SP nodes known about in deploy.yml
         """
+        logging.debug(self.__class__.__name__ + ': ' + sys._getframe().f_code.co_name)
         nodes = []
         for sp in self.get_sp_names():
             nodes.extend(self.get_sp_members(sp))
@@ -377,6 +398,7 @@ class ECSConf(object):
         """
         Returns a random node from the list of all known nodes
         """
+        logging.debug(self.__class__.__name__ + ': ' + sys._getframe().f_code.co_name)
         return random.SystemRandom().choice(self.list_all_sp_nodes())
 
     def get_sp_vdc(self, sp):
@@ -385,6 +407,7 @@ class ECSConf(object):
         :param sp: storage pool name
         :return: vdc name or None
         """
+        logging.debug(self.__class__.__name__ + ': ' + sys._getframe().f_code.co_name)
         for vdc in self.get_vdc_names():
             if sp in self.get_vdc_members(vdc):
                 return vdc
@@ -396,6 +419,7 @@ class ECSConf(object):
         Returns a list of names of storage pools in deploy.yml
         :returns: List of storage pool names
         """
+        logging.debug(self.__class__.__name__ + ': ' + sys._getframe().f_code.co_name)
         return self.get_names(SP)
 
     def get_sp_members(self, pool_name):
@@ -404,6 +428,7 @@ class ECSConf(object):
         :param pool_name: Name of the pool to list membership
         :returns: List of nodes in the pool
         """
+        logging.debug(self.__class__.__name__ + ': ' + sys._getframe().f_code.co_name)
         return self.get_members(SP, pool_name)
 
     def get_sp_options(self, pool_name):
@@ -412,6 +437,7 @@ class ECSConf(object):
         :param pool_name: Name of the pool
         :return: dict of pool options
         """
+        logging.debug(self.__class__.__name__ + ': ' + sys._getframe().f_code.co_name)
         opts = self.get_defaults(SP)
         sp_opts = self.get_attr(SP, OPTIONS, pool_name).toDict()
         if sp_opts is not None:
@@ -423,6 +449,7 @@ class ECSConf(object):
         Returns options only pertaining to the storage pool configuration within ECS
         :param pool_name: Name of the pool to report options on
         """
+        logging.debug(self.__class__.__name__ + ': ' + sys._getframe().f_code.co_name)
         return {x: y for x, y in self.get_sp_options(pool_name).items() if x in DEFAULTS[SP].keys()}
 
     def sp_ecs_options(self, pool_name):
@@ -430,6 +457,7 @@ class ECSConf(object):
         Returns options only pertaining to the storage pool configuration within ECS
         :param pool_name: Name of the pool to report options on
         """
+        logging.debug(self.__class__.__name__ + ': ' + sys._getframe().f_code.co_name)
         return {x: y for x, y in self.get_sp_options(pool_name).items() if x in DEFAULTS[SP].keys()}
 
     # Virtual Data Centers
@@ -437,12 +465,14 @@ class ECSConf(object):
         """
         Returns a list of names of all configured VDCs in the deployment
         """
+        logging.debug(self.__class__.__name__ + ': ' + sys._getframe().f_code.co_name)
         return self.get_names(VDC)
 
     def get_vdc_members(self, vdc_name):
         """
         Returns a list of the names of all storage pools assigned to the VDC
         """
+        logging.debug(self.__class__.__name__ + ': ' + sys._getframe().f_code.co_name)
         return self.get_members(VDC, vdc_name)
 
     def get_vdc_endpoint(self, vdc_name):
@@ -451,6 +481,7 @@ class ECSConf(object):
         :param vdc_name: VDC name string
         :return: top storage pool of the VDC
         """
+        logging.debug(self.__class__.__name__ + ': ' + sys._getframe().f_code.co_name)
         return self.get_sp_members(self.get_vdc_members(vdc_name)[0])[0]
 
     # def get_vdc_endpoint(self, vdc_name):
@@ -467,12 +498,14 @@ class ECSConf(object):
         Gets the top (first listed) VDC
         :return:
         """
+        logging.debug(self.__class__.__name__ + ': ' + sys._getframe().f_code.co_name)
         return self.get_vdc_names()[0]
 
     def get_new_vdc_secret(self, vdc_name):
         """
         Returns the configured VDC secret key, or None if no key is defined for the VDC
         """
+        logging.debug(self.__class__.__name__ + ': ' + sys._getframe().f_code.co_name)
         return self.get_attr(VDC, OPTIONS, vdc_name).secret_key
 
     def get_vdc_options(self, vdc_name):
@@ -481,6 +514,7 @@ class ECSConf(object):
         :param vdc_name: Name of the vdc
         :return: dict of vdc options
         """
+        logging.debug(self.__class__.__name__ + ': ' + sys._getframe().f_code.co_name)
         opts = self.get_defaults(VDC)
         vdc_opts = self.get_attr(VDC, OPTIONS, vdc_name).toDict()
         if vdc_opts is not None:
@@ -492,12 +526,14 @@ class ECSConf(object):
         """
         Returns a list of names of all replication groups in the configuration
         """
+        logging.debug(self.__class__.__name__ + ': ' + sys._getframe().f_code.co_name)
         return self.get_names(RG)
 
     def get_rg_members(self, rg_name):
         """
         Returns a list of all VDCs for the given replication group name
         """
+        logging.debug(self.__class__.__name__ + ': ' + sys._getframe().f_code.co_name)
         return self.get_members(RG, rg_name)
 
     def get_rg_options(self, rg_name):
@@ -506,6 +542,7 @@ class ECSConf(object):
         :param rg_name: Name of the rg
         :return: dict of rg options
         """
+        logging.debug(self.__class__.__name__ + ': ' + sys._getframe().f_code.co_name)
         opts = self.get_defaults(RG)
         rg_opts = self.get_attr(RG, OPTIONS, rg_name)
         if rg_opts is not None:
@@ -514,9 +551,11 @@ class ECSConf(object):
 
 # Namespaces
     def get_ns_names(self):
+        logging.debug(self.__class__.__name__ + ': ' + sys._getframe().f_code.co_name)
         return self.get_names(NS)
 
     def get_ns_users(self, ns_name):
+        logging.debug(self.__class__.__name__ + ': ' + sys._getframe().f_code.co_name)
         users = self.get_members(NS, ns_name, NAMESPACE_ADMINS)[0]
         if users is None:
             users = [NAMESPACE_ADMINS_DEFAULT]
@@ -528,6 +567,7 @@ class ECSConf(object):
         :param rg_name: Name of the rg
         :return: dict of rg options
         """
+        logging.debug(self.__class__.__name__ + ': ' + sys._getframe().f_code.co_name)
         opts = self.get_defaults(NS)
         ns_opts = self.get_attr(NS, OPTIONS, ns_name).toDict()
         if ns_opts is not None:
@@ -540,6 +580,7 @@ class ECSConf(object):
         :param ns_name: ns name
         :return:
         """
+        logging.debug(self.__class__.__name__ + ': ' + sys._getframe().f_code.co_name)
         return self.get_attr(NS, NAMESPACE_VPOOL, ns_name)
 
     def get_ns_dict(self, ns_name):
@@ -548,6 +589,7 @@ class ECSConf(object):
         :param ns_name:
         :return: ns_dict
         """
+        logging.debug(self.__class__.__name__ + ': ' + sys._getframe().f_code.co_name)
         ns_dict = {}
         ns_dict.update(self.get_ns_options(ns_name))
         ns_dict.update({NAMESPACE_ADMINS: self.get_ns_users(ns_name)})
@@ -555,9 +597,11 @@ class ECSConf(object):
         return ns_dict
 
     def get_mu_names(self):
+        logging.debug(self.__class__.__name__ + ': ' + sys._getframe().f_code.co_name)
         return self.get_names(MU, USERNAME)
 
     def get_mu_options(self, mu_name):
+        logging.debug(self.__class__.__name__ + ': ' + sys._getframe().f_code.co_name)
         opts = self.get_defaults(MU)
         mu_opts = self.get_attr(MU, OPTIONS, mu_name).toDict()
         if mu_opts is not None:
@@ -565,17 +609,21 @@ class ECSConf(object):
         return opts
 
     def get_mu_password(self, mu_name):
+        logging.debug(self.__class__.__name__ + ': ' + sys._getframe().f_code.co_name)
         return self.get_attr(MU, PASSWORD, mu_name)
 
     def get_mu_dict(self, mu_name):
+        logging.debug(self.__class__.__name__ + ': ' + sys._getframe().f_code.co_name)
         mu_dict = {}
         mu_dict.update(self.get_mu_options(mu_name))
         return mu_dict
 
     def get_ou_names(self):
+        logging.debug(self.__class__.__name__ + ': ' + sys._getframe().f_code.co_name)
         return self.get_names(OU, USERNAME)
 
     def get_ou_options(self, ou_name):
+        logging.debug(self.__class__.__name__ + ': ' + sys._getframe().f_code.co_name)
         opts = self.get_defaults(OU)
         ou_opts = self.get_attr(OU, OPTIONS, ou_name).toDict()
         if ou_opts is not None:
@@ -583,7 +631,9 @@ class ECSConf(object):
         return opts
 
     def get_ou_namespace(self, ou_name):
+        logging.debug(self.__class__.__name__ + ': ' + sys._getframe().f_code.co_name)
         return self.get_attr(OU, NAMESPACE, ou_name)
 
     def get_ou_dict(self, ou_name):
+        logging.debug(self.__class__.__name__ + ': ' + sys._getframe().f_code.co_name)
         return self.get_ou_options(ou_name)
