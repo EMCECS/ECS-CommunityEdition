a3b09b9c7f0920eb9f2520e8c7107884e7d832e1
Adrian Moreno <adrian@morenomartinez.com>
2017-06-30 15:12:42 +0200

    Allow multi-node configuration

--- tests/output.tf
+++ tests/output.tf
@@ -3,5 +3,5 @@ output "install_node_ip" {
 }
 
 output "ecs_node_ip" {
-  value = "${vsphere_virtual_machine.ecs_node.network_interface.0.ipv4_address}"
+  value = "${vsphere_virtual_machine.ecs_node.*.network_interface.0.ipv4_address}"
 }
