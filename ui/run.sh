1277f77dd65f1ead0d1742aaeacd44a7eac91de6
Travis Wichert <padthaitofuhot@gmail.com>
2017-11-17 16:15:22 -0500

    no longer check OVA for reboot requirement

--- ui/run.sh
+++ ui/run.sh
@@ -112,7 +112,7 @@ case "$(basename ${0})" in
         #run ecsdeploy load || exit $?
         run ecsdeploy cache || exit $?
     ;;
-    island-step2|ova-step1)
+    island-step2)
         #run ecsdeploy load || exit $?
         run ecsdeploy access || exit $?
         run ecsdeploy check || exit $?
@@ -120,6 +120,14 @@ case "$(basename ${0})" in
         run ecsdeploy reboot || exit $?
         sleep 10
         run ping_until_clear
+        run ecsdeploy deploy || exit $?
+        run ecsdeploy start || exit $?
+    ;;
+    ova-step1)
+        #run ecsdeploy load || exit $?
+        run ecsdeploy access || exit $?
+        run ecsdeploy check || exit $?
+        run ecsdeploy bootstrap || exit $?
         run ecsdeploy deploy || exit $?
         run ecsdeploy start || exit $?
     ;;
