6fd46a548db44cc97ad4d3522dfa00261677d4ab
Travis Wichert <padthaitofuhot@gmail.com>
2018-03-05 13:22:44 -0500

    bump versions

--- ui/setup.py
+++ ui/setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='ecsdeploy',
-    version='2.6.0',
+    version='2.6.1',
     packages=find_packages(),
     scripts=['ui.py',
              'ecsdeploy.py',
