fb0602727e85bf4413ba2adda0efb7fbd2c4cda3
Adrián Moreno <adrian@morenomartinez.com>
2018-04-05 23:15:44 +0200

    Update terraform configuration to work with new vsphere plugin (#405)
    * Update terraform configuration to work with new vsphere plugin
    
    * Update terraform configuration
    
    * Update terraform config
    
    * Update terraform config
    
    * Update terraform config
    
    * Update terraform config
    
    * Update terraform config
    
    * Update terraform config
    
    * Update tfconfig
    
    * Update tf config
    
    * Update tf config
    
    * Update tf config
    
    * Update tf config
    
    * Update tf
    
    * Update tf
    
    * update tf
    
    * Replace cache host by IP
    
    * Replace gotham cache IP by DNS name
    
    * Update centos mirror argument to cache DNS name
    
    * Replace deprecated tf field disk.name by disk.label
    
    * Update internal NTP server

--- tests/lab.tf
+++ tests/lab.tf
@@ -1,54 +1,107 @@
 provider "vsphere" {
-  user           = "${var.vsphere_user}"
-  password       = "${var.vsphere_password}"
-  vsphere_server = "${var.vsphere_server}"
-
+  user                 = "${var.vsphere_user}"
+  password             = "${var.vsphere_password}"
+  vsphere_server       = "${var.vsphere_server}"
   # if you have a self-signed cert
   allow_unverified_ssl = true
 }
 
+data "vsphere_datacenter" "dc" {
+  name               = "${var.datacenter}"
+}
+
+data "vsphere_datastore" "ds" {
+  name               = "${var.datastore}"
+  datacenter_id      = "${data.vsphere_datacenter.dc.id}"
+}
+
+data "vsphere_resource_pool" "pool" {
+  name               = "${var.resource_pool}"
+  datacenter_id      = "${data.vsphere_datacenter.dc.id}"
+}
+
+data "vsphere_network" "net" {
+  name               = "${var.network_interface}"
+  datacenter_id      = "${data.vsphere_datacenter.dc.id}"
+}
+
+data "vsphere_virtual_machine" "template" {
+  name               = "${var.template}"
+  datacenter_id      = "${data.vsphere_datacenter.dc.id}"
+}
+
 resource "vsphere_folder" "tests_folder" {
-  path = "tests"
-  datacenter = "${var.datacenter}"
+  path               = "ecsce-test-${substr(sha1(timestamp()),0,8)}"
+  type               = "vm"
+  datacenter_id      = "${data.vsphere_datacenter.dc.id}"
 }
 
 resource "vsphere_virtual_machine" "install_node" {
-  name   = "jenkins-ecsce-install-${substr(sha1(timestamp()),0,8)}"
-  folder = "${vsphere_folder.tests_folder.path}"
-  vcpu   = 2
-  memory = 4096
-  resource_pool = "${var.resource_pool}"
-  datacenter    = "${var.datacenter}"
-  skip_customization = true
+  name               = "jenkins-ecsce-install"
+  folder             = "${vsphere_folder.tests_folder.path}"
+  num_cpus           = 2
+  memory             = 4096
+  resource_pool_id   = "${data.vsphere_resource_pool.pool.id}"
+  datastore_id       = "${data.vsphere_datastore.ds.id}"
+  guest_id           = "${data.vsphere_virtual_machine.template.guest_id}"
+  scsi_type          = "${data.vsphere_virtual_machine.template.scsi_type}"
 
   network_interface {
-    label = "${var.network_interface}"
+    network_id       = "${data.vsphere_network.net.id}"
+    adapter_type     = "${data.vsphere_virtual_machine.template.network_interface_types[0]}"
   }
 
   disk {
-    template = "${var.template}"
-    datastore = "${var.datastore}"
-    type = "thin"
+    label            = "jenkins-ecsce-install.vmdk"
+    size             = "${data.vsphere_virtual_machine.template.disks.0.size}"
+    thin_provisioned = true
+    unit_number      = 0
+  }
+
+  disk {
+    label            = "jenkins-ecsce-install_1.vmdk"
+    size             = "${data.vsphere_virtual_machine.template.disks.1.size}"
+    thin_provisioned = true
+    unit_number      = 1
+  }
+
+  clone {
+    template_uuid    = "${data.vsphere_virtual_machine.template.id}"
   }
 }
 
 resource "vsphere_virtual_machine" "ecs_node" {
-  count  = "${var.ecs_nodes}"
-  name   = "jenkins-ecsce-ecs-${count.index}-${substr(sha1(timestamp()),0,8)}"
-  folder = "${vsphere_folder.tests_folder.path}"
-  vcpu   = 4
-  memory = 16384
-  resource_pool = "${var.resource_pool}"
-  datacenter    = "${var.datacenter}"
-  skip_customization = true
+  count              = "${var.ecs_nodes}"
+  name               = "jenkins-ecsce-ecs-${count.index}"
+  folder             = "${vsphere_folder.tests_folder.path}"
+  num_cpus           = 4
+  memory             = 16384
+  resource_pool_id   = "${data.vsphere_resource_pool.pool.id}"
+  datastore_id       = "${data.vsphere_datastore.ds.id}"
+  guest_id           = "${data.vsphere_virtual_machine.template.guest_id}"
+  scsi_type          = "${data.vsphere_virtual_machine.template.scsi_type}"
 
   network_interface {
-    label = "${var.network_interface}"
+    network_id       = "${data.vsphere_network.net.id}"
+    adapter_type     = "${data.vsphere_virtual_machine.template.network_interface_types[0]}"
   }
 
   disk {
-    template = "${var.template}"
-    datastore = "${var.datastore}"
-    type = "thin"
+    label            = "jenkins-ecsce-ecs-${count.index}.vmdk"
+    size             = "${data.vsphere_virtual_machine.template.disks.0.size}"
+    thin_provisioned = true
+    unit_number      = 0
+  }
+
+  disk {
+    label            = "jenkins-ecsce-ecs-${count.index}_1.vmdk"
+    size             = "${data.vsphere_virtual_machine.template.disks.1.size}"
+    thin_provisioned = true
+    unit_number      = 1
+  }
+
+  clone {
+    template_uuid    = "${data.vsphere_virtual_machine.template.id}"
+
   }
 }
--- tests/output.tf
+++ tests/output.tf
@@ -1,7 +1,7 @@
 output "install_node_ip" {
-  value = "${vsphere_virtual_machine.install_node.network_interface.0.ipv4_address}"
+  value = "${vsphere_virtual_machine.install_node.default_ip_address}"
 }
 
 output "ecs_node_ip" {
-  value = "${vsphere_virtual_machine.ecs_node.*.network_interface.0.ipv4_address}"
+  value = "${vsphere_virtual_machine.ecs_node.*.default_ip_address}"
 }
