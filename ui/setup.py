d68bff4155da1a43582f1311355e689f55967cab
Travis Wichert <padthaitofuhot@users.noreply.github.com>
2018-06-01 15:15:26 -0400

    bump version to 2.8.0r
    Took 9 minutes

--- ui/setup.py
+++ ui/setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='ecsdeploy',
-    version='2.8.0dev',
+    version='2.8.0r',
     packages=find_packages(),
     scripts=['ui.py',
              'ecsdeploy.py',
