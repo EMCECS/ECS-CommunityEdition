653b614293bccb0356d7f2300cd537981be183dd
Jason Cwik <jason.cwik@emc.com>
2020-03-24 16:01:14 -0500

    Updates for 3.5.0.0

--- ui/setup.py
+++ ui/setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='ecsdeploy',
-    version='2.10.0r',
+    version='2.9.3r',
     packages=find_packages(),
     scripts=['ui.py',
              'ecsdeploy.py',
