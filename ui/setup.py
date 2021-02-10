8ab9634d3663031ea837033230ad384f687d7331
Jason Cwik <jason.cwik@emc.com>
2021-02-10 14:53:13 -0600

    3.5.1.2 (#525)
    * Added patches to enable ECS 3.5.1.2
    * Added CentOS 7.9 support
    
    Co-authored-by: Jason Cwik <jason.cwik@dell.com>
    Co-authored-by: Steve Beckwith <steve.beckwith@emc.com>
    Co-authored-by: beckws <steve.beckwith@dell.com>
    Co-authored-by: Travis Wichert <travis.wichert@emc.com>

--- ui/setup.py
+++ ui/setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='ecsdeploy',
-    version='2.10.0r',
+    version='2.11.0r',
     packages=find_packages(),
     scripts=['ui.py',
              'ecsdeploy.py',
